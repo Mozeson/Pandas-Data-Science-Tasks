8b46a4873c5096b97167bf57fe6566c9665e8abe
Rajamim <Mikoex@gmail.com>
2022-11-27 13:03:44 +0200

    Added Jupyter Notebook

--- Sales-Analysis.py
+++ Sales-Analysis.py
@@ -4,6 +4,7 @@ import os
 df = pd.read_csv("SalesAnalysis\Sales_Data\Sales_April_2019.csv")
 files = [file for file in os.listdir('SalesAnalysis\Sales_Data')]
 
+all_months_data = pd.DataFrame(
 for file in files:
     print (file)
     
